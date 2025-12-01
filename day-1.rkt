871426a26b11c4b8f37d70d6598f8cfe5409d503
Barry Moore <chiroptical@gmail.com>
2025-12-01 10:04:06 -0500

    Day 1 part 2 still missing some edge case

--- day-1.rkt
+++ day-1.rkt
@@ -31,25 +31,27 @@
     password))
 
 (define (point-and-turns p amt)
-  (cons (new-point p amt) (quotient (+ p (abs amt)) 100)))
-
-; (require racket/trace)
-
-; (trace point-and-turns)
+  (let* ([direction (sgn amt)]
+         [total (+ p amt)]
+         [quot (quotient total 100)]
+         [next (new-point p amt)])
+    (if (and (not (= p 0)) (= direction -1) (= quot 0) (>= (abs amt) p))
+        (cons next 1)
+        (cons next (abs quot)))))
 
 (define (part-2 file)
-  (let*-values ([(lines) (read-notes file)]
-                [(_point password)
-                 (for/fold ([point 50]
-                            [password 0])
-                           ([line lines])
-                   (match (string->list line)
-                     [(cons #\R amt)
-                      (match (point-and-turns point (make-amount amt))
-                        [(cons new-point turns) (values new-point (+ password turns))])]
-                     [(cons #\L amt)
-                      (match (point-and-turns point (- (make-amount amt)))
-                        [(cons new-point turns) (values new-point (+ password turns))])]))])
+  (let* ([lines (read-notes file)]
+         [password (for/fold ([point 50]
+                              [password 0]
+                              #:result password)
+                             ([line lines])
+                     (match (string->list line)
+                       [(cons #\R amt)
+                        (match (point-and-turns point (make-amount amt))
+                          [(cons new-point turns) (values new-point (+ password turns))])]
+                       [(cons #\L amt)
+                        (match (point-and-turns point (- (make-amount amt)))
+                          [(cons new-point turns) (values new-point (+ password turns))])]))])
 
     password))
 
@@ -58,13 +60,18 @@
 
   (check-equal? (new-point 50 -68) 82 "returns the correct point")
 
+  (check-equal? (point-and-turns 55 -55) (cons 0 1))
+  (check-equal? (point-and-turns 1 -24) (cons 77 1))
   (check-equal? (point-and-turns 50 -68) (cons 82 1))
+  (check-equal? (point-and-turns 82 -30) (cons 52 0))
   (check-equal? (point-and-turns 98 102) (cons 0 2))
   (check-equal? (point-and-turns 95 60) (cons 55 1))
   (check-equal? (point-and-turns 50 1000) (cons 50 10))
+  (check-equal? (point-and-turns 50 -1000) (cons 50 9))
 
   (check-equal? (part-1 "inputs/day-1-test.txt") 3 "part 1 test")
   (check-equal? (part-1 "inputs/day-1.txt") 984 "part 1")
 
   (check-equal? (part-2 "inputs/day-1-test.txt") 6 "part 2 test")
-  (check-equal? (part-2 "inputs/day-1.txt") 5380 "part 2"))
+  ; TODO: Something is still wrong about point-and-turns...
+  (check-equal? (part-2 "inputs/day-1.txt") 5657 "part 2"))
