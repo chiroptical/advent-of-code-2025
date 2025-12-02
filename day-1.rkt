ac44087426c6568389f86e280efd083420051611
chiroptical <chiroptical@gmail.com>
2025-12-01 21:01:00 -0500

    Refactor for learning

--- day-1.rkt
+++ day-1.rkt
@@ -16,18 +16,20 @@
 (define (make-amount amt)
   (string->number (list->string amt)))
 
+(define/match (dir->sign _)
+  [(#\R) 1]
+  [(#\L) -1])
+
 (define (part-1 file)
-  (let*-values ([(lines) (read-notes file)]
-                [(_point password) (for/fold ([point 50]
-                                              [password 0])
-                                             ([line lines])
-                                     (match (string->list line)
-                                       [(cons #\R amt)
-                                        (let ([p (new-point point (make-amount amt))])
-                                          (values p (increment-password password p)))]
-                                       [(cons #\L amt)
-                                        (let ([p (new-point point (- (make-amount amt)))])
-                                          (values p (increment-password password p)))]))])
+  (let* ([lines (read-notes file)]
+         [password (for/fold ([point 50]
+                              [password 0]
+                              #:result password)
+                             ([line lines])
+                     (match-define (cons dir amt) (string->list line))
+                     (define p
+                       (new-point point (* (dir->sign dir) (make-amount amt))))
+                     (values p (increment-password password p)))])
     password))
 
 (define (point-and-turns p amt)
@@ -41,7 +43,8 @@
       [(and (zero? p) (negative? amt)) (cons next (quotient (abs amt) 100))]
 
       ; hit zero or summation is negative, crossed zero add 1 to quotient of the summation
-      [(or (zero? summation) (negative? summation)) (cons next (+ 1 (quotient (abs summation) 100)))]
+      [(or (zero? summation) (negative? summation))
+       (cons next (+ 1 (quotient (abs summation) 100)))]
 
       [else (cons next 0)])))
 
@@ -51,14 +54,11 @@
                               [password 0]
                               #:result password)
                              ([line lines])
-                     (match (string->list line)
-                       [(cons #\R amt)
-                        (match (point-and-turns point (make-amount amt))
-                          [(cons new-point turns) (values new-point (+ password turns))])]
-                       [(cons #\L amt)
-                        (match (point-and-turns point (- (make-amount amt)))
-                          [(cons new-point turns) (values new-point (+ password turns))])]))])
-
+                     (match-define (cons dir amt) (string->list line))
+                     (match-define (cons new-point turns)
+                       (point-and-turns point
+                                        (* (dir->sign dir) (make-amount amt))))
+                     (values new-point (+ password turns)))])
     password))
 
 (module+ test
@@ -81,5 +81,4 @@
   (check-equal? (part-1 "inputs/day-1.txt") 984 "part 1")
 
   (check-equal? (part-2 "inputs/day-1-test.txt") 6 "part 2 test")
-  ; TODO: Something is still wrong about point-and-turns...
   (check-equal? (part-2 "inputs/day-1.txt") 5657 "part 2"))
