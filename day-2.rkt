811306a4bd23877abdefc33c2a908451bb4c9ce7
Barry Moore <chiroptical@gmail.com>
2025-12-02 12:58:14 -0500

    Day 2, Part 2 solved but too slow

--- day-2.rkt
+++ day-2.rkt
@@ -2,7 +2,9 @@
 
 (provide part-1)
 
-(require "util.rkt")
+(require data/collection
+         "util.rkt"
+         racket/trace)
 
 ; TODO: macro which makes define -> dfn, match-define -> cases
 
@@ -10,7 +12,7 @@
   (match-define (cons begin (cons end _)) (string-split s "-"))
   (inclusive-range (string->number begin) (string->number end)))
 
-(define (is-invalid? n)
+(define (part-1? n)
   (define s (number->string n))
   (define l (string-length s))
   (define m (quotient l 2))
@@ -18,42 +20,57 @@
   (define snd-half (substring s m))
   (equal? fst-half snd-half))
 
-(require racket/list/grouping)
+(define (eq? x y)
+  (equal? (sequence->list* x) (sequence->list* y)))
 
-(require racket/trace)
+(define (seq-eq? heads tails)
+  (for/sequence ([h heads] [t tails]) (eq? h t)))
 
-(define/match (all-equal _)
-  [((cons hd tl)) (andmap (lambda (x) (equal? hd x)) tl)])
+(trace seq-eq?)
 
-(trace all-equal)
+; TODO: This is not sufficiently lazy
+(define (all-equal seq)
+  (define heads (repeat (first seq)))
+  (define tails (rest seq))
+  (andmap identity (seq-eq? heads tails)))
 
-; TODO: Use chunk from collections
-; https://docs.racket-lang.org/collections/collections-api.html#%28def._%28%28lib._data%2Fcollection..rkt%29._chunk%29%29
-(define (is-invalid-2? n)
+(define (part-2? n)
   (define s (number->string n))
   (define l (string-length s))
-  (define m (quotient l 2))
+  (define m (+ 1 (quotient l 2)))
   (for/fold ([is-invalid #f]) ([w (in-range 1 m)])
     #:break is-invalid
-    (define ws (windows w w (string->list s)))
-    (all-equal ws)))
+    (define seq (chunk w (string->list s)))
+    (all-equal seq)))
 
-(define (part-1 file)
+(define (solve is-inv? file)
   (define lines (file->lines file))
   (define ranges (csv (first lines)))
-  (for/fold ([acc 0]) ([range ranges])
-    (define ids (mk-range range))
-    (+ acc (foldl + 0 (filter is-invalid? ids)))))
+  (define futs
+    (for*/list ([range ranges]
+                [id (mk-range range)])
+      (future (thunk (if (is-inv? id) id 0)))))
+  (for/fold ([acc 0]) ([fut futs])
+    (+ acc (touch fut))))
+
+(define (part-1 file)
+  (solve part-1? file))
+
+(define (part-2 file)
+  (solve part-2? file))
 
 (module+ test
   (require rackunit)
 
-  (check-equal? (mk-range "11-13") '(11 12 13))
-  (check-equal? (is-invalid? 11) #t)
-  (check-equal? (is-invalid? 101) #f)
+  ; (check-equal? (mk-range "11-13") '(11 12 13))
+  ; (check-equal? (part-1? 11) #t)
+  ; (check-equal? (part-1? 101) #f)
 
-  (check-equal? (is-invalid-2? 2121212121) #t)
-  (check-equal? (is-invalid-2? 212121212) #f)
+  ; (check-equal? (part-2? 2121212121) #t)
+  (check-equal? (part-2? 212121212) #f)
 
-  (check-equal? (part-1 "inputs/day-2-test.txt") 1227775554)
-  (check-equal? (part-1 "inputs/day-2.txt") 19386344315))
+  ; (check-equal? (part-1 "inputs/day-2-test.txt") 1227775554)
+  ; (check-equal? (part-1 "inputs/day-2.txt") 19386344315)
+  ; (check-equal? (part-2 "inputs/day-2-test.txt") 4174379265)
+  ; (check-equal? (part-2 "inputs/day-2.txt") 34421651192)
+  )
