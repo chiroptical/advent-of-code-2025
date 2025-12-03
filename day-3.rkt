ff55f35b991d46672083a3b904951c7764862ec4
Barry Moore <chiroptical@gmail.com>
2025-12-03 16:43:43 -0500

    Finish day 3 part 2

--- day-3.rkt
+++ day-3.rkt
@@ -15,31 +15,44 @@
            (values x fx)
            (values to-return max-val))])))
 
-(define/match (pair->number _)
-  [((cons x y)) (+ (* 10 x) y)])
+(define (list->number xs)
+  (define rev (reverse xs))
+  (for/fold ([acc 0]
+             [multiplier 1]
+             #:result acc)
+            ([v rev])
+    (values (+ acc (* multiplier v)) (* 10 multiplier))))
 
-(define/match (shift _pair _next)
-  [((cons x y) z) (max-by pair->number (list (cons x y) (cons x z) (cons y z)))])
+(define (shift xs next size)
+  (max-by list->number (combinations (append xs (list next)) size)))
 
-(define (part-1 file)
+(define (solve file size)
   (define lines (file->lines file))
   (for/fold ([sum 0]) ([l lines])
     (define numbers (map char->number (string->list l)))
     (define final
-      (for/fold ([acc (cons 0 0)]) ([n numbers])
-        (shift acc n)))
-    (+ sum (pair->number final))))
+      (for/fold ([acc (make-list size 0)]) ([n numbers])
+        (shift acc n size)))
+    (+ sum (list->number final))))
+
+(define (part-1 file)
+  (solve file 2))
+
+(define (part-2 file)
+  (solve file 12))
 
 (module+ test
   (require rackunit)
 
-  (check-equal? (shift (cons 9 8) 7) (cons 9 8))
-  (check-equal? (shift (cons 7 6) 8) (cons 7 8))
-  (check-equal? (shift (cons 8 6) 7) (cons 8 7))
-  (check-equal? (shift (cons 6 8) 7) (cons 8 7))
-  (check-equal? (shift (cons 5 6) 4) (cons 6 4))
-  (check-equal? (shift (cons 8 7) 8) (cons 8 8))
-  (check-equal? (shift (cons 8 9) 2) (cons 9 2))
+  (check-equal? (shift (list 9 8) 7 2) (list 9 8))
+  (check-equal? (shift (list 7 6) 8 2) (list 7 8))
+  (check-equal? (shift (list 8 6) 7 2) (list 8 7))
+  (check-equal? (shift (list 6 8) 7 2) (list 8 7))
+  (check-equal? (shift (list 5 6) 4 2) (list 6 4))
+  (check-equal? (shift (list 8 7) 8 2) (list 8 8))
+  (check-equal? (shift (list 8 9) 2 2) (list 9 2))
 
   (check-equal? (part-1 "inputs/day-3-test.txt") 357)
-  (check-equal? (part-1 "inputs/day-3.txt") 17412))
+  (check-equal? (part-1 "inputs/day-3.txt") 17412)
+  (check-equal? (part-2 "inputs/day-3-test.txt") 3121910778619)
+  (check-equal? (part-2 "inputs/day-3.txt") 172681562473501))
