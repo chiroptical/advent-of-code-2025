5d909162f479efb1c03259a4d9bd34a4af9e69a5
chiroptical <chiroptical@gmail.com>
2025-12-03 07:38:08 -0500

    Day 3 part 1: candidate

--- day-3.rkt
+++ day-3.rkt
@@ -6,11 +6,21 @@
 (cases (shift _pair _next)
        [((cons #f #f) z) (cons z #f)]
        [((cons x #f) z) (cons x z)]
-       [((cons x y) z) #:when (and (< z x) (> z y)) (cons x z)]
-       [((cons x y) z) #:when (and (> z x) (< z y)) (cons y z)]
+       [((cons x y) z) #:when (or (> z x) (> z y)) (cons (max x y) z)]
        [((cons x y) _) (cons x y)])
 
-(defn (part-1 file) (defn lines (file->lines file)) 0)
+(defn (part-1 file)
+      (defn lines (file->lines file))
+      (for/fold ([sum 0]) ([l lines])
+        (defn numbers (map char->number (string->list l)))
+        (defn final
+              (for/fold ([acc (cons #f #f)]) ([n numbers])
+                (shift acc n)))
+        (defn f (first final))
+        (defn s (second final))
+        (defn num-s (string-append (number->string f) (number->string s)))
+        (defn num (string->number num-s))
+        (+ sum num)))
 
 (module+ test
   (require rackunit)
@@ -22,5 +32,7 @@
   (check-equal? (shift (cons 8 6) 7) (cons 8 7))
   (check-equal? (shift (cons 6 8) 7) (cons 8 7))
   (check-equal? (shift (cons 5 6) 4) (cons 5 6))
+  (check-equal? (shift (cons 8 7) 8) (cons 8 8))
 
-  (check-equal? (part-1 "inputs/day-3-test.txt" 357)))
+  ;(check-equal? (part-1 "inputs/day-3-test.txt") 357)
+  )
