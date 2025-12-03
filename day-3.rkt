deb71523b67222467ef2ba6751d89ebbcf74fc5c
chiroptical <chiroptical@gmail.com>
2025-12-03 10:00:22 -0500

    Day 1 part 3, misinterpreted goal added TODO/tests

--- day-3.rkt
+++ day-3.rkt
@@ -3,6 +3,11 @@
 (require "util.rkt"
          racket/trace)
 
+(cases (pair->number _) [((cons x y)) (+ (* 10 x) y)])
+
+; TODO: misinterpreted this function, we need the max of all combinations
+; i.e. (x y) (x z) (y z)
+; Additionally, we shouldn't start from (#f #f), just use (0 0)
 (cases (shift _pair _next)
        [((cons #f #f) z) (cons z #f)]
        [((cons x #f) z) (cons x z)]
@@ -16,23 +21,17 @@
         (defn final
               (for/fold ([acc (cons #f #f)]) ([n numbers])
                 (shift acc n)))
-        (defn f (first final))
-        (defn s (second final))
-        (defn num-s (string-append (number->string f) (number->string s)))
-        (defn num (string->number num-s))
-        (+ sum num)))
+        (+ sum (pair->number final))))
 
 (module+ test
   (require rackunit)
 
-  (check-equal? (shift (cons #f #f) 1) (cons 1 #f))
-  (check-equal? (shift (cons 1 #f) 2) (cons 1 2))
-  (check-equal? (shift (cons 6 7) 8) (cons 7 8))
+  (check-equal? (shift (cons 9 8) 7) (cons 9 8))
   (check-equal? (shift (cons 7 6) 8) (cons 7 8))
   (check-equal? (shift (cons 8 6) 7) (cons 8 7))
   (check-equal? (shift (cons 6 8) 7) (cons 8 7))
-  (check-equal? (shift (cons 5 6) 4) (cons 5 6))
+  (check-equal? (shift (cons 5 6) 4) (cons 6 4))
   (check-equal? (shift (cons 8 7) 8) (cons 8 8))
+  (check-equal? (shift (cons 8 9) 2) (cons 9 2))
 
-  ;(check-equal? (part-1 "inputs/day-3-test.txt") 357)
-  )
+  (check-equal? (part-1 "inputs/day-3-test.txt") 357))
