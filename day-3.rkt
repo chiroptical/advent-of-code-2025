5a6a143c50f8fce914f459c8230a00b1b5fd12c7
Barry Moore <chiroptical@gmail.com>
2025-12-03 15:06:46 -0500

    Finish day 3 part 1

--- day-3.rkt
+++ day-3.rkt
@@ -1,27 +1,34 @@
 #lang racket
 
-(require "util.rkt"
-         racket/trace)
+(require "util.rkt")
 
-(cases (pair->number _) [((cons x y)) (+ (* 10 x) y)])
+(define (max-by f xs)
+  (for/fold ([to-return #f]
+             [max-val #f]
+             #:result to-return)
+            ([x xs])
+    (define fx (f x))
+    (match max-val
+      [#f (values x fx)]
+      [_
+       (if (> fx max-val)
+           (values x fx)
+           (values to-return max-val))])))
 
-; TODO: misinterpreted this function, we need the max of all combinations
-; i.e. (x y) (x z) (y z)
-; Additionally, we shouldn't start from (#f #f), just use (0 0)
-(cases (shift _pair _next)
-       [((cons #f #f) z) (cons z #f)]
-       [((cons x #f) z) (cons x z)]
-       [((cons x y) z) #:when (or (> z x) (> z y)) (cons (max x y) z)]
-       [((cons x y) _) (cons x y)])
+(define/match (pair->number _)
+  [((cons x y)) (+ (* 10 x) y)])
 
-(defn (part-1 file)
-      (defn lines (file->lines file))
-      (for/fold ([sum 0]) ([l lines])
-        (defn numbers (map char->number (string->list l)))
-        (defn final
-              (for/fold ([acc (cons #f #f)]) ([n numbers])
-                (shift acc n)))
-        (+ sum (pair->number final))))
+(define/match (shift _pair _next)
+  [((cons x y) z) (max-by pair->number (list (cons x y) (cons x z) (cons y z)))])
+
+(define (part-1 file)
+  (define lines (file->lines file))
+  (for/fold ([sum 0]) ([l lines])
+    (define numbers (map char->number (string->list l)))
+    (define final
+      (for/fold ([acc (cons 0 0)]) ([n numbers])
+        (shift acc n)))
+    (+ sum (pair->number final))))
 
 (module+ test
   (require rackunit)
@@ -34,4 +41,5 @@
   (check-equal? (shift (cons 8 7) 8) (cons 8 8))
   (check-equal? (shift (cons 8 9) 2) (cons 9 2))
 
-  (check-equal? (part-1 "inputs/day-3-test.txt") 357))
+  (check-equal? (part-1 "inputs/day-3-test.txt") 357)
+  (check-equal? (part-1 "inputs/day-3.txt") 17412))
