9104d2f1eeede228ca3feac4cb0a52a9d52e0544
chiroptical <chiroptical@gmail.com>
2025-12-05 07:57:27 -0500

    Finish day 4 part 2

--- day-4.rkt
+++ day-4.rkt
@@ -19,41 +19,43 @@
              (set-add acc (point r c)))])
     (set-remove s (point 0 0))))
 
-(define (part-1 file)
-  (define lines (file->lines file))
-  (define rolls (parse-lines lines))
-  (for/fold ([acc 0]) ([r rolls])
+(define (remove rolls)
+  (for/fold ([acc (set)]) ([r rolls])
     (define count
       (for/fold ([c 0]) ([k kernel])
         (if (set-member? rolls (add-points r k))
             (+ 1 c)
             c)))
     (if (< count 4)
-        (+ 1 acc)
+        (set-add acc r)
         acc)))
+
+(define (part-1 file)
+  (define lines (file->lines file))
+  (define rolls (parse-lines lines))
+  (define removed (remove rolls))
+  (set-count removed))
 
 (define (part-2 file)
   (define lines (file->lines file))
   (define rolls (parse-lines lines))
-  (define removals
-    (for/fold ([init rolls]
-               [final (set)]
+  (define final
+    (for/fold ([acc rolls]
                [terminate #f]
-               #:result final)
-              ([x (in-naturals)])
-      ; TODO: remove (> x 100)
-      #:break (or (> x 100) terminate)
-      ; TODO:
-      ; - for part-1, abstraction which returns set of points to remove
-      ; - remove those points from init
-      ; - if init and final are the same terminate
-      (values init final #f)))
-  (set-count removals))
+               #:result acc)
+              ([_ (in-naturals)])
+      #:break terminate
+      (define removed (remove acc))
+      (if (= (set-count removed) 0)
+          (values acc #t)
+          (values (set-subtract acc removed) #f))))
+  (- (set-count rolls) (set-count final)))
 
 (module+ test
   (require rackunit)
 
   (check-equal? (part-1 "inputs/day-4-test.txt") 13)
-  (check-equal? (part-1 "inputs/day-4.txt") 13)
+  (check-equal? (part-1 "inputs/day-4.txt") 1363)
 
-  (check-equal? (part-2 "inputs/day-4-test.txt") 43))
+  (check-equal? (part-2 "inputs/day-4-test.txt") 43)
+  (check-equal? (part-2 "inputs/day-4.txt") 8184))
