4bb93a7c3af9123e1a9a0a042712cc702efea0f3
Barry Moore <chiroptical@gmail.com>
2025-12-09 14:00:22 -0500

    Day 8, part 1 progress

--- day-8.rkt
+++ day-8.rkt
@@ -2,14 +2,16 @@
 
 (define (parse file)
   (define lines (file->lines file))
-  #f)
+  (for/fold ([junctions (list)]) ([l lines])
+    (match-define (list x y z) (string-split l ","))
+    (cons (junction (string->number x) (string->number y) (string->number z)) junctions)))
 
 (struct junction (x y z) #:transparent)
 
 (define (distance a b)
   (match-define (junction x1 y1 z1) a)
   (match-define (junction x2 y2 z2) b)
-  (sqrt (sqr (- x2 x1)) (sqr (- y2 y1)) (sqr (- z2 z1))))
+  (sqrt (+ (sqr (- x2 x1)) (sqr (- y2 y1)) (sqr (- z2 z1)))))
 
 (define (next x y los)
   (match-define-values (passthrough found-x found-y)
@@ -38,12 +40,30 @@
 ;   * zip (in-range n) distances
 ;   * next
 (define (part-1 file n)
-  (define input (parse file))
-  0)
+  (define junctions (parse file))
+  (define sorted-pairs
+    (sort (combinations junctions 2)
+          (lambda (a b)
+            (match-define (list x y) a)
+            (match-define (list m n) b)
+            (< (distance x y) (distance m n)))))
+  (define final-junctions
+    (for/fold ([acc (list)])
+              ([_ (in-range n)]
+               [pair sorted-pairs])
+      (match-define (list x y) pair)
+      (if (empty? acc)
+          (list (set x y))
+          (next x y acc))))
+  (println final-junctions)
+  0
+  ;(define sizes (sort (map set-count final-junctions)))
+  ;(foldl + 0 (take sizes 3)
+  )
 
 (module+ test
   (require rackunit)
 
-  (check-equal? (part-1 "inputs/day-8-test.txt" 10) 0)
+  (check-equal? (part-1 "inputs/day-8-test.txt" 10) 40)
   ; (check-equal? (part-1 "inputs/day-8.txt" 1000) 0)
   )
